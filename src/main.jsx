8f9bfb6b9bca7c49eca4edec7858d245896ab3ef
SaraErbismann <85120519+SaraErbismann@users.noreply.github.com>
2024-03-25 21:00:31 +0200

    Home, About, and Contact components. Links to App component, createBrowseRouter and RouteProvider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <Home />,
+        index: true
+      },
+      {
+        path: "about",
+        element: <About />,
+      }, 
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+    <RouterProvider/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
